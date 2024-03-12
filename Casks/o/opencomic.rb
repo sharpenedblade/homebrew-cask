638a11072a043aebfb60f746cef03ad64635c254
sharpenedblade <sharpenedblade@proton.me>
2024-03-11 23:52:18 -0700

    Add aarch64 cask for opencomic

--- Casks/o/opencomic.rb
+++ Casks/o/opencomic.rb
@@ -1,8 +1,19 @@
 cask "opencomic" do
-  version "1.1.0"
-  sha256 "98d0b314162c398f6f99dd2e0c6cdeb01e6940db830d7ea610a998818ecc2bd4"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://github.com/ollm/OpenComic/releases/download/v#{version}/OpenComic-#{version}.dmg"
+  version "1.1.0"
+
+  on_arm do
+    sha256 "4f8dc4b405990d5d0447ed87379c2a13d70144f48c78ee700f5316e8cd5a429e"
+
+    url "https://github.com/ollm/OpenComic/releases/download/v#{version}/OpenComic-#{version}-arm64.dmg"
+  end
+  on_intel do
+    sha256 "98d0b314162c398f6f99dd2e0c6cdeb01e6940db830d7ea610a998818ecc2bd4"
+
+    url "https://github.com/ollm/OpenComic/releases/download/v#{version}/OpenComic-#{version}.dmg"
+  end
+
   name "OpenComic"
   desc "Comic and Manga reader"
   homepage "https://github.com/ollm/OpenComic"
